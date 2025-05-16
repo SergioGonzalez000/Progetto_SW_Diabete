311569f77beba9def42f33aa0aeb0b35af2d1a69
Srj <serigon84@hotmail.it>
2025-05-16 15:53:55 +0200

    pattern MVC impostato

---
+++ bozze_codice/view.py
@@ -0,0 +1,30 @@
+from dash import dcc, html
+import dash_bootstrap_components as dbc
+
+def getLayout():
+    return dbc.Container(
+        [
+            html.H1(
+                "Welcome Nigga", 
+                className="text-center my-4"),
+            html.P(
+                "Bozza brutta giusto per", 
+                className="text-center"),
+            html.Div(
+                dbc.ButtonGroup([
+                    dbc.Button("Paziente", color="primary"), 
+                    dbc.Button("Diabetologo", color="danger")],
+                    size="md",
+                    className="mt-4",
+                    vertical=True),
+                className="d-flex justify-content-center" 
+            ),
+            dbc.Button(
+                "Accedi", 
+                href="/login", 
+                color="secondary", 
+                className="d-block mx-auto mt-3")
+        ],
+        fluid=True,
+        className="p-4"
+    )
