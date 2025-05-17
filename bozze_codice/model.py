c59299ca949f778f0277e04454fbb3c288838b18
filloferro33@gmail.com <filloferro33@gmail.com>
2025-05-17 18:49:46 +0200

    Aggiunta model
    connessione database

--- bozze_codice/model.py
+++ bozze_codice/model.py
@@ -1,1 +1,18 @@
 # gestione Database remoto, gestione altra roba logica
+
+import psycopg2
+
+
+connection = psycopg2.connect(
+    host='aws-0-eu-central-2.pooler.supabase.com',
+    dbname='postgres',
+    user='postgres.dozqdfylbqeriitzoblm',
+    password='IOtRbsJmgylEH5Pl',
+    port='5432'
+)
+
+cur=connection.cursor()
+
+def get_balls():
+    cur.execute("SELECT * FROM palle")
+    
