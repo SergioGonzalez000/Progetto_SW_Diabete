d808b6354e353734eb387fe7e453f65616835d77
filloferro33@gmail.com <filloferro33@gmail.com>
2025-05-21 19:03:00 +0200

    aggiunta genera_username e approva_richiesta

--- bozze_codice/model.py
+++ bozze_codice/model.py
@@ -1,4 +1,5 @@
 # gestione Database remoto, gestione altra roba logica
+from abc import ABC,abstractmethod
 
 import psycopg2
 
@@ -12,6 +13,11 @@ connection = psycopg2.connect(
 )
 
 cur=connection.cursor()
+
+class autenticabile():
+    pass
+        
+    
 
 class Persona():
     def __init__(self):
@@ -29,6 +35,48 @@ class Admin(Persona):
     def __init__(self):
         super().__init__()
 
+    def genera_username(id_richiesta):
+        cur.execute("SELECT * FROM RichiesteAccount WHERE id_richiesta = %s ",(id_richiesta,))
+        richiesta = cur.fetchone()
+        nome=richiesta[1]
+        cognome=richiesta[2]
+        paziente=richiesta[12]
+
+        if paziente:
+            cur.execute("SELECT * FROM Paziente WHERE nome = %s AND cognome = %s",(nome,cognome))
+            righe = cur.fetchall()
+            num=len(righe)
+            username=f"{nome}.{cognome}{num}_P"
+        else:
+            cur.execute("SELECT * FROM Diabetologo WHERE nome = %s AND  cognome = %s",(nome,cognome))
+            righe = cur.fetchall()
+            num=righe.count()
+            username=f"{nome}.{cognome}{num}_D"
+
+        return username
+
+
+    def approva_richiesta(id_richiesta):
+        cur.execute("SELECT * FROM RichiesteAccount WHERE id_richiesta = %s ",(id_richiesta,))
+        richiesta = cur.fetchone()
+        _,nome, cognome, data_nascita, sesso, codice_fiscale, indirizzo, citta, cap, telefono, email, flag_paziente, _,_, password=richiesta
+        if flag_paziente:
+            username=Admin.genera_username(id_richiesta)
+            cur.execute("""INSERT INTO Paziente 
+                    (nome, cognome, data_nascita, sesso, codice_fiscale, indirizzo, citta, cap, telefono, email, username, pw) 
+                    VALUES (%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, %s)""", 
+                    (nome, cognome, data_nascita, sesso, codice_fiscale, indirizzo, citta, cap, telefono, email, username, password))
+            connection.commit()
+        else:
+            username=Admin.genera_username(id_richiesta)
+            cur.execute("""INSERT INTO Diabetologo 
+                    (nome, cognome, data_nascita, sesso, codice_fiscale, indirizzo, citta, cap, telefono, email, username, pw) 
+                    VALUES (%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, %s)""", 
+                    (nome, cognome, data_nascita, sesso, codice_fiscale, indirizzo, citta, cap, telefono, email, username, password))
+            connection.commit()
+
+
+
 class Terapia():
     def __init__(self):
         pass
@@ -41,5 +89,5 @@ class Glicemia():
     def __init__(self):
         pass
 
-
-        
+if __name__ == '__main__':
+    Admin.approva_richiesta(1)
