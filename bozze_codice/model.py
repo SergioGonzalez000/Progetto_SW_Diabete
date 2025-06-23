76d4b1f0fb7bdb332717fddae6f0cc8d220e9d62
filloferro33@gmail.com <filloferro33@gmail.com>
2025-06-23 21:21:30 +0200

    modifiche per far funzionare tutto

--- bozze_codice/model.py
+++ bozze_codice/model.py
@@ -964,7 +964,7 @@ def visualizza_media_glicemia_per_diabetologi():
     medie = []
 
     for d in diabetologi:
-        id_d = d["id"]
+        id_d = d["id_diabetologo"]
         cursore.execute("SELECT * FROM Diabetologo WHERE id_diabetologo=%s",(id_d,))
         r=cursore.fetchone()
         Diab=Diabetologo(r[1],r[2],r[3],r[4],r[5],r[6],r[7],r[8],r[9],r[10],r[11],r[12])
