05e267e1104b4edd9c32edb8d56e0427c5d16b70
filloferro33@gmail.com <filloferro33@gmail.com>
2025-07-12 17:09:54 +0200

    test completo e cambiamenti minori

--- bozze_codice/model.py
+++ bozze_codice/model.py
@@ -479,7 +479,7 @@ class Admin(Persona):
         with DBSingleton.get_cursor() as cursore:
             cursore.execute("""INSERT INTO Paziente 
                         (nome, cognome, data_nascita, sesso, codice_fiscale, indirizzo, citta, cap, telefono, email, username, pw) 
-                        VALUES (%s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s, %s)""", 
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""", 
                         (p.nome, p.cognome, p.data_nascita, p.sesso, p.cf, p.indirizzo, p.citta, p.cap, p.tel, p.email, p.username, p.pw))
             
 
